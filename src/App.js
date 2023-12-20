78af35906871942ae680d4a152ac0419d92c8554
Amey Bhandari <bhandariamey@gmail.com>
2023-12-20 10:44:18 +0530

    trying card component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
-import { useState } from 'react';
+import Card from './components/Card/Card.jsx';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
   const [searchData, useSearchData] = useState()
+  const [albumData, setAlbumData] = useState([])
+
+  const callAlbumApi = async()=>{
+    try{
+      console.log("API call")
+      const API = "https://qtify-backend-labs.crio.do/albums/top"
+      const response = await axios.get(API)
+      if(response.status === 200){
+        console.log(response.data);
+        setAlbumData(response.data)
+      }
+    }
+      catch(e){
+        console.log("Error fetching data");
+      }
+  }
+  
+  useEffect(()=>{
+    callAlbumApi()
+  },[])
 
   return (
     <>
       <Navbar searchData={searchData}/>
       <Hero/>
+      {albumData.length>0 && <Card data={albumData} type={'album'}/>}
     </>
   );
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,64 @@
+import {Chip, Tooltip} from '@mui/material';
+import styles from './Card.module.css'
+
+export default function Card({data,type}){
+    console.log(data);
+    const getCard = (type)=>{
+        switch(type){
+            case 'album' :  { 
+                     let { image, title, follows, slug, songs } = data[0];
+                    return (
+                      <>
+                        <Tooltip title={`${songs.length}`} placement="top" arrow>
+                          <a href={`/album/${slug}`}>
+                            <div className={styles.Wrapper}>
+                              <div className={styles.Card}>
+                                <img src={image} alt="song" loading="lazy" />
+                                <div className={styles.Banner}>
+                                  <Chip
+                                    label={`${follows} Follows`}
+                                    size="small"
+                                    className={styles.Chip}
+                                  />
+                                </div>
+                              </div>
+                              <div className={styles.titleWrapper}>
+                                <p>{title}</p>
+                              </div>
+                            </div>
+                          </a>
+                        </Tooltip>
+                      </>
+                    );
+                  
+            }
+
+            case 'song' :  {
+                const {likes,image,title} = data;
+                return(<>
+                    <div className={styles.Wrapper}>
+                        <div className={styles.Card}>
+                            <img src={image} alt="song" loading="lazy" />
+                            <div className={styles.Banner}>
+                                <div className={styles.Pill}>
+                                    <p>{likes} Likes</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.titleWrapper}> 
+                            <p>{title}</p>
+                        </div>
+                    </div>
+                </>)
+                }
+
+            default:
+                 return <></>
+        }
+
+    }
+    
+     return (
+       getCard(type)
+)
+}
